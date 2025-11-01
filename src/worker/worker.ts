12fddd38268ff1c3bd7c8c48ed4cf04841678cd2
Hanna Salameh <hannasalameh9@gmail.com>
2025-11-01 20:07:10 +0200

    edit

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -59,7 +59,7 @@ async function analayzDomains(domain : string) {
 // CONCURRENCY = 2
 
 /*
-Execution:
+Execution:1111
 	1.	Worker picks A and B → runs them in parallel
 	2.	Once A finishes → pick C
 	3.	Once B finishes → pick D
